dec98690d1b6ca087403707d33b2eb8f0821196a
Cris Machado <cris@machnet.ca>
2024-09-07 16:54:33 -0400

    initial commit variableFun assignment

---
+++ variableFun.py
@@ -0,0 +1,17 @@
+query1 = 5 * 6
+print("The answer to your query is:", query1)
+
+query2 = 8 / 3
+print("The answer to your query is:", round(query2, 8))
+
+query3 = 8 // 3
+print("The answer to your query is:", query3)
+
+query4 = 8 % 3
+print("The answer to your query is:", query4)
+
+query5 = 4200000 + 23090
+print("The answer to your query is:", query5)
+
+query6 = 4200000 - 7633
+print("The answer to your query is:", query6)
